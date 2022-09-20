5bbfd0267f323872ae46e8bd0bd2e52b72929fce
ZIZUdesign <kahsayzeray@gmail.com>
2022-09-20 15:10:33 +0200

    React added

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -13,14 +13,13 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-       
+
         private readonly StoreContext _context;
         
         public ProductsController(StoreContext context)
         {
             _context = context;
-            
-            
+    
         }
 
         [HttpGet]
@@ -36,6 +35,6 @@ namespace API.Controllers
         {
             return await _context.Products.FindAsync(id);
         }
-        
+    
     }
 }
